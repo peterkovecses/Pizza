1e4d0d2956ed060136bb5fe1cc2a6fc8194318f8
Peter Kovecses <kovecsespeter@gmail.com>
2022-07-03 14:36:34 +0200

    Implement basic Console logging.

--- Pizza.Api/Controllers/ProductsController.cs
+++ Pizza.Api/Controllers/ProductsController.cs
@@ -15,11 +15,13 @@ namespace Pizza.Api.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
+        private readonly ILogger<ProductsController> _logger;
 
-        public ProductsController(IProductService productService, ICategoryService categoryService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, ILogger<ProductsController> logger)
         {
             _productService = productService;
             _categoryService = categoryService;
+            _logger = logger;
         }
 
         [ApiVersion("1.0")]
@@ -27,6 +29,8 @@ namespace Pizza.Api.Controllers
         [ResponseCache(Duration = 60, VaryByQueryKeys = new[] { "MinPrice", "MaxPrice", "SearchTerm", "CategoryId", "PageNumber", "PageSize", "SortBy", "SortOrder" })]
         public async Task<IActionResult> GetProductsAsync_V1_0([FromQuery] ProductQueryParameters queryParameters)
         {
+            _logger.LogInformation($"Run endpoint /api/product Get ");
+
             var products = await _productService.GetProductsAsync_V1_0(queryParameters);
 
             var response = Ok(products);            
@@ -38,7 +42,9 @@ namespace Pizza.Api.Controllers
         [HttpGet]
         [ResponseCache(Duration = 60, VaryByQueryKeys = new[] { "MinPrice", "MaxPrice", "SearchTerm", "CategoryId", "PageNumber", "PageSize", "SortBy", "SortOrder" })]
         public async Task<IActionResult> GetProductsAsync_V2_0([FromQuery] ProductQueryParameters queryParameters)
-        {            
+        {   
+            _logger.LogInformation($"Run endpoint /api/product Get ");
+
             var products = await _productService.GetProductsAsync_V2_0(queryParameters);
 
             Response.AddPaginationHeader(products.CurrentPage, products.PageSize,
@@ -51,10 +57,15 @@ namespace Pizza.Api.Controllers
         [ResponseCache(Duration = 60)]
         public async Task<IActionResult> GetProductAsync(int id)
         {
+            _logger.LogInformation($"Run endpoint /api/product/{id} Get ");
+
             var product = await _productService.GetProductAsync(id);
 
             if (product == null)
+            {
+                _logger.LogTrace($"Failed to get Product entity with Id {product.Id}, because it doesn't exist.");
                 return NotFound();
+            }
 
             return Ok(product);
         }
@@ -62,13 +73,23 @@ namespace Pizza.Api.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateProductAsync(ProductDto productDto)
         {
+            _logger.LogInformation("Run endpoint /api/product POST ");
+
             if (!ModelState.IsValid)
+            {
+                _logger.LogTrace($"Failed to create Product, because ModelState not valid.");
                 return BadRequest(ModelState);
+            }
 
             if (!_categoryService.IsCategoryExists(productDto.CategoryId).Result)
+            {
+                _logger.LogTrace($"Failed to create Product, because Category with Id {productDto.CategoryId} doesn't exist.");
                 return NotFound("The specified category does not exist.");
+            }
 
             productDto.Id = await _productService.CreateProductAsync(productDto);
+
+            _logger.LogTrace($"Added new Product entity with Id {productDto.Id}");
 
             return CreatedAtAction("GetProduct", new { id = productDto.Id }, productDto);
         }
@@ -76,33 +97,57 @@ namespace Pizza.Api.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateProductAsync(int id, ProductDto productDto)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+            _logger.LogInformation($"Run endpoint /api/product/{id} PUT ");
 
-            if (id != productDto.Id)
+            if (!ModelState.IsValid)
+            {
+                _logger.LogTrace($"Failed to upgrade Product entity with Id {productDto.Id}, because ModelState not valid.");
                 return BadRequest(ModelState);
+            }
+            if (id != productDto.Id)
+            {
+                _logger.LogTrace($"Failed to upgrade Product entity with Id {productDto.Id}, because Ids are not the same.");
+                return BadRequest(ModelState);
+            }
 
             try
             {
                 await _productService.UpdateProductAsync(productDto);
+
+                _logger.LogTrace($"Succesfully updated Product entity with Id {productDto.Id}");
+
                 return Ok(productDto);
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!await _productService.IsProductExists(id))
+                {
+                    _logger.LogTrace($"Failed to update Product entity with Id {productDto.Id}, because Product doesn't exist.");
                     return NotFound();
+                }
                 else
+                {
+                    _logger.LogTrace($"Failed to update Product entity with Id {productDto.Id} for an unknown reason");
                     return BadRequest();
+                }                
             }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProductAsync(int id)
         {
+            _logger.LogInformation($"Run endpoint /api/product/{id} Delete ");
+
             if (!await _productService.IsProductExists(id))
+            {
+                _logger.LogTrace($"Failed to delete Product entity with Id {id}, because Product doesn't exist.");
                 return NotFound();
+            }
 
             var product = await _productService.DeleteProductAsync(id);
+
+            _logger.LogTrace($"Succesfully deleted Product entity with Id {product.Id}");
+
             return Ok(product);
         }
 
@@ -110,6 +155,8 @@ namespace Pizza.Api.Controllers
         [Route("Delete")]
         public async Task<IActionResult> DeleteProductsAsync([FromQuery] int[] ids)
         {
+            _logger.LogInformation($"Run endpoint /api/product/Delete Post ");
+
             foreach (var id in ids)
             {
                 if (!await _productService.IsProductExists(id))
@@ -117,6 +164,9 @@ namespace Pizza.Api.Controllers
             }
 
             var products = await _productService.DeleteProductsAsync(ids);
+
+            _logger.LogTrace($"Succesfully deleted Product entities.");
+
             return Ok(products);
         }
     }
--- Pizza.Bll/Services/ProductService.cs
+++ Pizza.Bll/Services/ProductService.cs
@@ -7,6 +7,8 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using System.Linq.Expressions;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 
 namespace Pizza.Bll.Services
 {
@@ -15,12 +17,16 @@ namespace Pizza.Bll.Services
         private readonly PizzaDbContext _dbContext;
         private readonly IMapper _mapper;
         private readonly IMemoryCache _memoryCache;
+        private readonly ILogger<ProductService> _logger;
+        private readonly Stopwatch _stopwatch;
 
-        public ProductService(PizzaDbContext dbContext, IMapper mapper, IMemoryCache memoryCache)
+        public ProductService(PizzaDbContext dbContext, IMapper mapper, IMemoryCache memoryCache, ILogger<ProductService> logger)
         {
             _dbContext = dbContext;
             _mapper = mapper;
             _memoryCache = memoryCache;
+            _logger = logger;
+            _stopwatch = new Stopwatch();
             _dbContext.Database.EnsureCreated();
         }
 
@@ -34,39 +40,63 @@ namespace Pizza.Bll.Services
             CategoryId = p.CategoryId
         };
 
+        private void LogProductServiceStarted()
+        {
+            _logger.LogInformation("Product Background Service - Start");
+            _stopwatch.Start();
+        }
+
+        private void LogProductServiceEnded()
+        {
+            _stopwatch?.Stop();
+            _logger.LogInformation($"Product Background Service finished in {_stopwatch.Elapsed.TotalSeconds} seconds.");
+        }
+
         public async Task<int> CreateProductAsync(ProductDto productDto)
         {
+            LogProductServiceStarted();
+
             var product = _mapper.Map<ProductDto, Product>(productDto);
 
             _dbContext.Products.Add(product);
             await _dbContext.SaveChangesAsync();
 
+            LogProductServiceEnded();
             return product.Id;
         }
 
         public async Task<ProductDto> DeleteProductAsync(int id)
         {
+            LogProductServiceStarted();
+
             var product = await _dbContext.Products.SingleAsync(p => p.Id == id);
 
             product.IsDeleted = true;
 
             await _dbContext.SaveChangesAsync();
 
+            LogProductServiceEnded();
+
             return _mapper.Map<Product, ProductDto>(product);
         }
 
         public async Task<IEnumerable<ProductDto>> DeleteProductsAsync(int[] ids)
         {
+            LogProductServiceStarted();
+
             var productsToDelete = _dbContext.Products.Where(p => ids.Contains(p.Id));
             await productsToDelete.ForEachAsync(p => p.IsDeleted = true);            
 
             await _dbContext.SaveChangesAsync();
 
+            LogProductServiceEnded();
             return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDto>>(productsToDelete);
         }
 
         public async Task<ProductDto> GetProductAsync(int id)
         {
+            LogProductServiceStarted();
+
             var product = _memoryCache.Get<Product>(id);
 
             if (product == null)
@@ -78,11 +108,15 @@ namespace Pizza.Bll.Services
                 _memoryCache.Set(id, product, cacheEntryOptions);
             }
 
+            LogProductServiceEnded();
+
             return _mapper.Map<ProductDto>(product);
         }
 
         public async Task<IEnumerable<ProductDto>> GetProductsAsync_V1_0(ProductQueryParameters queryParameters)
         {
+            LogProductServiceStarted();
+
             var products = await _dbContext.Products
                 .Where(p => p.IsDeleted == false)
                 .FilterCategory(queryParameters.CategoryId)
@@ -93,32 +127,50 @@ namespace Pizza.Bll.Services
                 .Take(queryParameters.PageSize)
                 .ToListAsync();
 
+            LogProductServiceEnded();
+
             return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDto>>(products);
         }
         public async Task<PagedList<ProductDto>> GetProductsAsync_V2_0(ProductQueryParameters queryParameters)
         {
-            return await _dbContext.Products
+            LogProductServiceStarted();
+
+            var products = await _dbContext.Products
                 .Where(p => p.IsDeleted == false)
                 .Select(_productSelector)
                 .FilterCategory(queryParameters.CategoryId)
                 .FilterByPrice(queryParameters.MinPrice, queryParameters.MaxPrice)
                 .SearchByTerm(queryParameters.SearchTerm)
                 .OrderProductByCustom(queryParameters.SortBy, queryParameters.SortOrder)
-                .ToPagedListAsync<ProductDto>(queryParameters.PageNumber, queryParameters.PageSize);                
+                .ToPagedListAsync<ProductDto>(queryParameters.PageNumber, queryParameters.PageSize);
+
+            LogProductServiceEnded();
+
+            return products;
         }
 
         public async Task<bool> IsProductExists(int id)
         {
-            return await _dbContext.Products.AnyAsync(b => b.Id == id);
+            LogProductServiceStarted();
+
+            var exists = await _dbContext.Products.AnyAsync(b => b.Id == id);
+
+            LogProductServiceEnded();
+
+            return exists;
         }
 
         public async Task UpdateProductAsync(ProductDto productDto)
         {
+            LogProductServiceStarted();
+
             var productInDb = await _dbContext.Products.SingleAsync(p => p.Id == productDto.Id);
 
             _mapper.Map(productDto, productInDb);
 
             await _dbContext.SaveChangesAsync();
+
+            LogProductServiceEnded();
         }
     }
 }
